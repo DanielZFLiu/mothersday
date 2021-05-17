0546c0c5f3ac19546177a2b81b4a23f254434292
DanielZFLiu <daniel.zf.liu@gmail.com>
2021-05-17 00:04:46 -0400

    built and deployed

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,6 @@
     <img class="background" id="background8" src="..\src\assets\油墨画8.png" alt="">
     <img class="background" id="boat" src="@\assets\小舟.png" alt="">
     <span class="paragraph" id="msg">{{message}}</span>
-    <!-- <div class="click"></div> -->
-    <!-- <p class="paragraph">每人出生就是画手，负责画出自己或幸福或平淡或悲惨的一生。
-      如果这么想的话，母亲一定就是我们画画的老师。
-      以刘子枫、刘诗秋为名的画卷上，初始的部分是精细入微的画，
-      洋溢着饱满又温暖的颜色。</p> -->
-    
-    <!-- <p>妈，非常感谢您。接下来的就要我们尽可能单独去画了；
-      但向您保证，我们会画出精彩的作品来。</p> -->
-
-    <!-- <p>洗衣做饭忙不完，每日忙率不着闲，
-      历经风霜添白发，勤劳持家不知烦，
-      尝尽苦辣皆为甜，养大子女无怨言。
-      祝福您健康长寿，愿生活中最美好的幸福永远属于您。</p> -->
     <router-view/>
   </div>
 </template>
@@ -32,11 +19,6 @@ export default{
   data(){
     return{
       message: "",
-      // msg1: "每人出生就是画手，负责画出自己或幸福或平淡或悲惨的一生。\
-      // 如果这么想的话，母亲一定就是我们画画的老师。\
-      // 以刘子枫、刘诗秋为名的画卷上，初始的部分是精细入微的画，\
-      // 洋溢着饱满又温暖的颜色。",
-      // i: 0
     }
   },
   methods: {
@@ -121,61 +103,62 @@ export default{
     padding: 0 20px; 
 
     font-family: Roboto, "Open Sans", "Helvetica Neue";
+    z-index: 0;
   }
 
   @keyframes background1 {
-    0% {opacity: 0%;}
-    50% {opacity: 100%;}
-    100% {opacity: 100%;}
+    0% {opacity: 0;}
+    50% {opacity: 1;}
+    100% {opacity: 1;}
   }
 
   @keyframes background2 {
-    0% {opacity: 0%;}
-    56% {opacity: 0%;}
-    62% {opacity: 100%;}
-    100% {opacity: 100%;}
+    0% {opacity: 0;}
+    56% {opacity: 0;}
+    62% {opacity: 1;}
+    100% {opacity: 1;}
   }
 
   @keyframes background3 {
-    0% {opacity: 0%;}
-    62% {opacity: 0%;}
-    68% {opacity: 100%;}
-    100% {opacity: 100%;}
+    0% {opacity: 0;}
+    62% {opacity: 0;}
+    68% {opacity: 1;}
+    100% {opacity: 1;}
   }
 
   @keyframes background4 {
-    0% {opacity: 0%;}
-    68% {opacity: 0%;}
-    74% {opacity: 100%;}
-    100% {opacity: 100%;}
+    0% {opacity: 0;}
+    68% {opacity: 0;}
+    74% {opacity: 1;}
+    100% {opacity: 1;}
   }
 
   @keyframes background5 {
-    0% {opacity: 0%;}
-    74% {opacity: 0%;}
-    80% {opacity: 100%;}
-    100% {opacity: 100%;}
+    0% {opacity: 0;}
+    74% {opacity: 0;}
+    80% {opacity: 1;}
+    100% {opacity: 1;}
   }
 
   @keyframes background6 {
-    0% {opacity: 0%;}
-    80% {opacity: 0%;}
-    86% {opacity: 100%;}
-    100% {opacity: 100%;}
+    0% {opacity: 0;}
+    80% {opacity: 0;}
+    86% {opacity: 1;}
+    100% {opacity: 1;}
   }
 
   @keyframes background7 {
-    0% {opacity: 0%;}
-    86% {opacity: 0%;}
-    92% {opacity: 100%;}
-    100% {opacity: 100%;}
+    0% {opacity: 0;}
+    86% {opacity: 0;}
+    92% {opacity: 1;}
+    100% {opacity: 1;}
   }
 
   @keyframes background8 {
-    0% {opacity: 0%;}
-    92% {opacity: 0%;}
-    98% {opacity: 100%;}
-    100% {opacity: 100%;}
+    0% {opacity: 0;}
+    92% {opacity: 0;}
+    98% {opacity: 1;}
+    100% {opacity: 1;}
   }
 
   .background{
@@ -188,7 +171,7 @@ export default{
 
   #background1{
     z-index: 1;
-    opacity: 0%;
+    opacity: 0;
     animation-name: background1;
     animation-duration: 3s;
     animation-fill-mode: forwards;
@@ -196,7 +179,7 @@ export default{
 
   #background2{
     z-index: 2;
-    opacity: 0%;
+    opacity: 0;
     animation-name: background2;
     animation-duration: 3s;
     animation-fill-mode: forwards;
@@ -204,7 +187,7 @@ export default{
 
   #background3{
     z-index: 3;
-    opacity: 0%;
+    opacity: 0;
     animation-name: background3;
     animation-duration: 3s;
     animation-fill-mode: forwards;
@@ -212,7 +195,7 @@ export default{
 
   #background4{
     z-index: 4;
-    opacity: 0%;
+    opacity: 0;
     animation-name: background4;
     animation-duration: 3s;
     animation-fill-mode: forwards;
@@ -220,7 +203,7 @@ export default{
 
   #background5{
     z-index: 5;
-    opacity: 0%;
+    opacity: 0;
     animation-name: background5;
     animation-duration: 3s;
     animation-fill-mode: forwards;
@@ -228,7 +211,7 @@ export default{
 
   #background6{
     z-index: 6;
-    opacity: 0%;
+    opacity: 0;
     animation-name: background6;
     animation-duration: 3s;
     animation-fill-mode: forwards;
@@ -236,7 +219,7 @@ export default{
 
   #background7{
     z-index: 7;
-    opacity: 0%;
+    opacity: 0;
     animation-name: background7;
     animation-duration: 3s;
     animation-fill-mode: forwards;
@@ -244,20 +227,20 @@ export default{
 
   #background8{
     z-index: 8;
-    opacity: 0%;
+    opacity: 0;
     animation-name: background8;
     animation-duration: 3s;
     animation-fill-mode: forwards;
   }
 
   @keyframes rowing {
-    0% {left: -10%; opacity: 0%;}
-    10% {left: -10%; opacity: 100%;}
-    100% {left: 10%; opacity: 100%;}
+    0% {left: -10%; opacity: 0;}
+    10% {left: -10%; opacity: 1;}
+    100% {left: 10%; opacity: 1;}
   }
 
   #boat{
-    opacity: 0%;
+    opacity: 0;
     z-index: 9;
     animation-name: rowing;
     animation-duration: 10s;
@@ -265,14 +248,4 @@ export default{
     animation-delay: 18000ms;
     animation-timing-function: linear;
   }
-
-  /* .click{
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    background-color: transparent;
-    z-index: 100;
-  } */
 </style>
